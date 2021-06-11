f7dde570661bfdef1d33b2bc72c666019505db18
Marcelo <marcelo.a.marcal@hotmail.com>
2021-06-11 14:25:56 -0300

    Validando o cadastro de categoria

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -23,7 +23,17 @@ class CategoriesRepository {
       description,
       created_at: new Date(),
     });
+
     this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name);
+    return category;
   }
 }
 
